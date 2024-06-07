cbd70b55589c1be5deacf1c683566aabbbe3b6d6
ADMIN <ADMIN@PC224>
2024-06-07 08:54:53 +0700

    Add project files.

---
+++ WebBanHang/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace WebBanHang.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Chưa nhập dữ liệu"), StringLength(50)]
+        public String Name { get; set; }
+        [Range(1, 100), Required(ErrorMessage = "Chưa nhập giá  ")]
+        public int DisplayOrder { get; set; }
+    }
+}
